f1c0275d9ed69006920ce5629de0e3452a5283db
Edvin Tronnberg <edvin.tronnberg@gmail.com>
2020-09-29 18:11:41 +0200

    add generateRow method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,34 @@
 package com.company;
 
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        LinkedHashSet<Integer> myRow = generateRow();
+
+
+        for (Integer integer : myRow) {
+            System.out.println(integer);
+        }
+
+        
+    }
+
+    private static LinkedHashSet<Integer> generateRow() {
+        Random rand = new Random();
+        LinkedHashSet<Integer> myRow = new LinkedHashSet<>();
+
+        int bounds;
+        while (myRow.size() < 7) {
+
+            bounds = myRow.size() < 5 ? 100 : 10;
+            int nextRand = rand.nextInt(bounds) + 1;
+            myRow.add(nextRand);
+        }
+        return myRow;
     }
 }
